67b69eb031e74b1502c664d1adf0e8f2966545bb
Doug Anson <doug.anson@arm.com>
2026-07-03 12:49:31 -0500

    minor fix

--- prep1.sh
+++ prep1.sh
@@ -10,6 +10,7 @@ sudo apt -y autoclean
 sudo add-apt-repository -y ppa:deadsnakes/ppa
 sudo apt update
 sudo apt install -y python3.10-dev python3.10 python3.10-venv
+cd $HOME
 python3.10 -m venv rally
 echo "source $HOME/rally/bin/activate" >> $HOME/.bashrc
 echo "export JAVA21_HOME=/usr/lib/jvm/java-21-openjdk-arm64" >> $HOME/.bashrc
